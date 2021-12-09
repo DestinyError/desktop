e10cd3b9d0e77c1a3a37115083fec20dd1b447cf
DestinyError <95519399+DestinyError@users.noreply.github.com>
2021-12-09 22:25:17 +0700

    Update prj.java

--- prj.java
+++ prj.java
@@ -1,7 +1,7 @@
 public class prj {
   public static void main(String args[]) {
     System.out.print("Hello world");
-    System.out.print("New1");
+    System.out.print("Edit1");
     System.out.print("Edit2");
   }
 }
